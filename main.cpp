4c67470aacda4550302374f41d31302a2d2c0248
Avadanei Rares <avadaneirares0@gmail.com>
2026-03-30 02:21:32 +0300

    v.02
    Wrap main logic in a try-catch block to handle exceptions.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include "SpaceAgency.h"
- 
+#include <iostream>
+#include <exception>
+
 int main() {
-    SpaceAgency agency;
-    
-    agency.initializePlanets("planets.txt");
-    agency.initializeFleet("spaceships.txt");
-    agency.initializeAliens("aliens.txt");
-    agency.initializeUnknowns("unknowns.txt"); 
-    
-    agency.runCLI(); 
+    try {
+        SpaceAgency agency;
+        
+        agency.initializePlanets("planets.txt");
+        agency.initializeFleet("spaceships.txt");
+        agency.initializeAliens("aliens.txt");
+        agency.initializeUnknowns("unknowns.txt"); 
+       
+        agency.runCLI(); 
+        
     } catch (const std::exception& e) {
         std::cerr << "\n[CRASH SYSTEM] Eroare critica prinsa in main: " << e.what() << "\n";
     }
- 
+    
     return 0;
 }
