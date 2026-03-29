e4e9d0ec6b9e8edaa6d7419599918942086b9087
Avadanei Rares <avadaneirares0@gmail.com>
2026-03-30 01:31:04 +0300

    Add SpaceAgency class header file

---
+++ include/SpaceAgency.h
@@ -0,0 +1,32 @@
+#ifndef SPACEAGENCY_H
+#define SPACEAGENCY_H
+
+#include <vector>
+#include <string>
+#include <memory>
+#include "Spaceship.h"
+
+class SpaceAgency {
+private:
+    std::vector<std::shared_ptr<Spaceship>> fleet;
+    std::vector<Planet> planets; 
+    std::vector<Alien> aliens; 
+    std::vector<UnknownEntity> unknowns; 
+
+    static bool checkIntersection(int x1, int y1, int x2, int y2, int cx, int cy, int R);
+    void validateLocation(int targetX, int targetY, const std::string& newEntityName) const;
+
+public:
+    static int totalMissionsExecuted;
+    static double hqVault; 
+
+    void initializeFleet(const std::string& filename);
+    void initializePlanets(const std::string& filename);
+    void initializeAliens(const std::string& filename);
+    void initializeUnknowns(const std::string& filename);
+    
+    void runGlobalMissionReport();
+    void showStatus() const;
+};
+
+#endif // SPACEAGENCY_H
